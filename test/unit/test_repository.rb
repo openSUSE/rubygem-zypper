1112d9517e74b3a1f5ea0d4d378e0e12f9986dc3
Lukas Ocilka <lukas.ocilka@gmail.com>
2012-07-26 12:11:03 +0000

    Extending the repos test

--- test/unit/test_repository.rb
+++ test/unit/test_repository.rb
@@ -9,6 +9,8 @@ puts "Testing Zypper::Repository methods..."
 class TestRepository < Test::Unit::TestCase
   include TestHelper
 
+  WEBYAST_UPDATES_REPO_NAME = 'SLE11-WebYaST-SP2-Updates'
+
   def setup
     @repo = Zypper::Repository.new
   end
@@ -21,5 +23,12 @@ class TestRepository < Test::Unit::TestCase
     prepare_data('repos', 'xml')
 
     assert_equal(19, @repo.all.size)
+  end
+
+  def test_find
+    prepare_data('repos', 'xml')
+
+    repos_found = @repo.all.select{|repo| repo[:name] == WEBYAST_UPDATES_REPO_NAME}
+    assert_equal(1, repos_found.size)
   end
 end
