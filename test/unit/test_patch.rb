6185d96a7658b40905ff7ef6a221ff252404b89e
Lukas Ocilka <lukas.ocilka@gmail.com>
2012-07-27 09:10:46 +0000

    Tests for Patch.find completed

--- test/unit/test_patch.rb
+++ test/unit/test_patch.rb
@@ -56,49 +56,40 @@ class TestPatch < Test::Unit::TestCase
   def test_find
     prepare_data('list-updates-patches', 'xml')
 
-    assert_equal(1136, @patch.find(
-      :status => Zypper::Patch::Status::NOT_APPLICABLE
-    ).size)
-
-    assert_equal(11, @patch.find(
-      :status => Zypper::Patch::Status::NOT_APPLICABLE,
-      :name => 'sdksp1-libopenssl-devel'
-    ).size)
-
-    assert_equal(718, @patch.find(
-      :category => Zypper::Patch::Category::RECOMMENDED
-    ).size)
-
-    assert_equal(25, @patch.find(
-      :category => Zypper::Patch::Category::OPTIONAL
-    ).size)
-
-    assert_equal(2, @patch.find(:edition => '3970').size)
-
-    assert_equal(767, @patch.find(
-      :repository_url => 'https://nu.novell.com/repo/$RCE/SLES11-SP1-Updates/sle-11-x86_64?credentials=NCCcredentials'
-    ).size)
-
-    assert_equal(8, @patch.find(
-      :repository_url => 'https://nu.novell.com/repo/$RCE/SLES11-SP1-Updates/sle-11-x86_64?credentials=NCCcredentials',
-      :name => 'slessp1-apache2-mod_php5'
-    ).size)
-
-    assert_equal(1, @patch.find(
-      :repository_alias => 'nu_novell_com:SLES11-SP1-Updates',
-      :edition => '3970'
-    ).size)
-
-    assert_equal(27, @patch.find(
-      :restart => true
-    ).size)
-
     for test_definition in [
       [8,    {:status => Zypper::Patch::Status::NEEDED}],
       [187,  {:status => Zypper::Patch::Status::INSTALLED}],
       [1136, {:status => Zypper::Patch::Status::NOT_APPLICABLE}],
+
+      [718,  {:category => Zypper::Patch::Category::RECOMMENDED}],
+      [25,   {:category => Zypper::Patch::Category::OPTIONAL}],
+      [556,  {:category => Zypper::Patch::Category::SECURITY}],
+      [32,   {:category => Zypper::Patch::Category::FEATURE}],
+
+      [2,    {:edition => '3970'}],
+      [1,    {:edition => '3970', :repository_alias => 'nu_novell_com:SLES11-SP1-Updates'}],
+
+      [767,  {:repository_url => 'https://nu.novell.com/repo/$RCE/SLES11-SP1-Updates/sle-11-x86_64?credentials=NCCcredentials'}],
+      [8,    {:name => 'slessp1-apache2-mod_php5', :repository_url => 'https://nu.novell.com/repo/$RCE/SLES11-SP1-Updates/sle-11-x86_64?credentials=NCCcredentials'}],
+
+      [11,   {:status => Zypper::Patch::Status::NOT_APPLICABLE, :name => 'sdksp1-libopenssl-devel'}],
+
+      [27,   {:restart => true}],
+      [1304, {:restart => false}],
+
+      [1331, {:arch => 'noarch'}],
+
+      [16,   {:pkgmanager => true}],
+      [1315, {:pkgmanager => false}],
+
+      [27,   {:interactive => true}],
+      [1304, {:interactive => false}],
     ]
-      assert_equal(test_definition[0], @patch.find(test_definition[1]).size, "Testing #{test_definition[1].inspect}")
+      assert_equal(
+        test_definition[0],
+        @patch.find(test_definition[1]).size,
+        "Testing #{test_definition[1].inspect}"
+      )
     end
   end
 end
